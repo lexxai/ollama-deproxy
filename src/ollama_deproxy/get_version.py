7dcbcd704942a1477180274d7767c21b7f8725fe
LeX Xai <lexxai@users.noreply.github.com>
2026-03-28 03:50:24 +0200

    0.4.2 (#17)
    * Tune README.md

--- src/ollama_deproxy/get_version.py
+++ src/ollama_deproxy/get_version.py
@@ -1,9 +1,12 @@
+from . import __version__
+
+
 def app_version():
     from pathlib import Path
 
     BASE_PATH = Path(__file__).parent.parent
 
-    v = "0.0.1"
+    v = __version__ or "0.0.1"
     try:
         pyproject_file = BASE_PATH.parent / "pyproject.toml"
         if pyproject_file.exists():
